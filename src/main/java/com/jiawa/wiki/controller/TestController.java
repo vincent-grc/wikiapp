fabad1308b6ad107c9d84ab3ad4434164ad62e62
vincent81 <rc.guo020801@outlook.com>
2024-09-03 15:11:25 +0800

    add ComponentScan in WikiApplication use HTTP client to check GET and Post

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,13 +1,21 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "你好";
     }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "hello post" + name;
+    }
+
 }
