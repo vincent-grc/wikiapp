2f2693e0cdfd5354bece83c9f20607253d4b2082
vincent81 <rc.guo020801@outlook.com>
2025-03-08 00:21:33 -0500

    Build the-footer component.

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -53,14 +53,3 @@
   </a-layout-content>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-});
-</script>
