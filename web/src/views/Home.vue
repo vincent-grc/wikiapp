1ed1da376edfca13ac26b9898ad2dcbee9e3dc46
vincent81 <rc.guo020801@outlook.com>
2025-03-08 16:05:25 -0500

    Integrate icons component, $ npm install npm install --save @ant-design/icons-vue

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -45,36 +45,85 @@
         </a-menu>
       </a-layout-sider>
       <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-        <pre>
-          {{ebooks}}
-        </pre>
+        <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+          <template #footer>
+            <div>
+              <b>ant design vue</b>
+              footer part
+            </div>
+          </template>
+          <template #renderItem="{ item }">
+            <a-list-item key="item.title">
+              <template #actions>
+          <span v-for="{ icon, text } in actions" :key="icon">
+            <component :is="icon" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+              </template>
+              <template #extra>
+                <img
+                    width="272"
+                    alt="logo"
+                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                />
+              </template>
+              <a-list-item-meta :description="item.description">
+                <template #title>
+                  <a :href="item.href">{{ item.title }}</a>
+                </template>
+                <template #avatar><a-avatar :src="item.avatar" /></template>
+              </a-list-item-meta>
+              {{ item.content }}
+            </a-list-item>
+          </template>
+        </a-list>
       </a-layout-content>
     </a-layout>
   </a-layout-content>
 </template>
 
-<script lang="ts">
-import { defineComponent, onMounted, ref} from 'vue';
+<script setup lang="ts">
+import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
+import { onMounted, ref } from 'vue';
 import axios from "axios";
 
-export default defineComponent({
-  name: 'Home',
-  setup() {
-    console.log("Set up");
-    const ebooks = ref();
+// Define listData directly (automatically available in template)
+const listData: Record<string, string>[] = [];
 
-    onMounted(() => {
-      console.log("onMounted ");
-      axios.get("http://localhost:8080/ebook/list?name=Spring").then((response) => {
-        const data = response.data;
-        ebooks.value = data.content;
-        console.log(response);
-      });
-    });
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
-    return {
-      ebooks
-    };
-  }
+const pagination = {
+  onChange: (page: number) => {
+    console.log(page);
+  },
+  pageSize: 3,
+};
+const actions: Record<string, any>[] = [
+  { icon: StarOutlined, text: '156' },
+  { icon: LikeOutlined, text: '156' },
+  { icon: MessageOutlined, text: '2' },
+];
+
+// Reactive data
+const ebooks = ref();  // Explicitly set an array type
+
+// Fetch data on component mount
+onMounted(() => {
+  console.log("onMounted ");
+  axios.get("http://localhost:8080/ebook/list?name=Spring").then((response) => {
+    const data = response.data;
+    ebooks.value = data.content;  // Ensure `ebooks.value` is assigned
+    console.log(response);
+  });
 });
 </script>
