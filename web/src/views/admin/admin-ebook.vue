9c9d7ce7b550ec4052928e3ebd47f1c86c0111a1
vincent81 <rc.guo020801@outlook.com>
2025-03-09 15:30:55 -0400

    Update Ebook manage page with data representation.

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -2,12 +2,119 @@
   <a-layout-content style="padding: 0 50px">
     <a-layout style="padding: 24px 0; background: #fff">
       <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-        <div class="AdminEbook">
-          <h1>Manage Ebook</h1>
-        </div>
+        <a-table
+            :columns="columns"
+            :row-key="record => record.id"
+            :data-source="ebooks"
+            :pagination="pagination"
+            :loading="loading"
+            @change="handleTableChange"
+        >
+          <template #cover="{ text: cover }">
+            <img v-if="cover" :src="cover" alt="avatar" />
+          </template>
+          <template v-slot:action="{ text, record }">
+            <a-space size="small">
+              <a-button type="primary">
+                Edit
+              </a-button>
+              <a-button type="danger">
+                Delete
+              </a-button>
+            </a-space>
+          </template>
+        </a-table>
       </a-layout-content>
     </a-layout>
   </a-layout-content>
 </template>
-<script setup lang="ts">
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'AdminEbook',
+  setup() {
+    const ebooks = ref();
+    const pagination = ref({
+      current: 1,
+      pageSize: 2,
+      total: 0
+    });
+    const loading = ref(false);
+
+    const columns = [
+      {
+        title: 'cover',
+        dataIndex: 'cover',
+        slots: { customRender: 'cover' }
+      },
+      {
+        title: 'name',
+        dataIndex: 'name'
+      },
+      {
+        title: 'category',
+        slots: { customRender: 'category' }
+      },
+      {
+        title: 'doc_count',
+        dataIndex: 'docCount'
+      },
+      {
+        title: 'view_count',
+        dataIndex: 'viewCount'
+      },
+      {
+        title: 'vote_count',
+        dataIndex: 'voteCount'
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        slots: { customRender: 'action' }
+      }
+    ];
+
+    /**
+     * Data query
+     **/
+    const handleQuery = (params: any) => {
+      loading.value = true;
+      // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+      axios.get("/ebook/list", params).then((response) => {
+        loading.value = false;
+        const data = response.data;
+        ebooks.value = data.content;
+
+        // 重置分页按钮
+        pagination.value.current = params.page;
+      });
+    };
+
+    /**
+     * 表格点击页码时触发
+     */
+    const handleTableChange = (pagination: any) => {
+      console.log("看看自带的分页参数都有啥：" + pagination);
+      handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      });
+    };
+
+    onMounted(() => {
+      handleQuery({});
+    });
+
+    return {
+      ebooks,
+      pagination,
+      columns,
+      loading,
+      handleTableChange,
+    }
+  }
+});
 </script>
