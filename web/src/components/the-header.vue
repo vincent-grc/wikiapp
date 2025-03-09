8bb8dc2941eb9d5c266fd82ae04c741f5a708318
vincent81 <rc.guo020801@outlook.com>
2025-03-09 13:40:50 -0400

    Add Ebook manage page. New page create process: Page, Router, Menu

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -6,9 +6,15 @@
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 111</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
+      <a-menu-item key="1">
+        <router-link to="/">Home</router-link>
+      </a-menu-item>
+      <a-menu-item key="2">
+        <router-link to="/admin/ebook">Manage Ebook</router-link>
+      </a-menu-item>
+      <a-menu-item key="3">
+        <router-link to="/about">About me</router-link>
+      </a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -2,10 +2,12 @@
   <a-layout-content style="padding: 0 50px">
     <a-layout style="padding: 24px 0; background: #fff">
       <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-        <div class="About">
-          <h1>This is an about page lol</h1>
+        <div class="AdminEbook">
+          <h1>Manage Ebook</h1>
         </div>
       </a-layout-content>
     </a-layout>
   </a-layout-content>
 </template>
+<script setup lang="ts">
+</script>
