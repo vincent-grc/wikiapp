19f35cb122789fd4066bebfdbb772682fb2bcd1e
vincent81 <rc.guo020801@outlook.com>
2025-03-10 09:55:30 -0400

    Integrate PageHelper plugin, change mapper log level to trace.

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -1,6 +1,8 @@
 package com.jiawa.wiki.service;
 
 import ch.qos.logback.core.util.StringUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jiawa.wiki.domain.Ebook;
 import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
@@ -8,6 +10,8 @@ import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
 import com.jiawa.wiki.utils.CopyUtil;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -18,6 +22,8 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     //Both annotation are valid
     @Resource
     //@Autowired
@@ -26,11 +32,16 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req) {
         EbookExample example = new EbookExample();
         EbookExample.Criteria criteria = example.createCriteria();
+
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
-
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(example);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("Total rows: {}", pageInfo.getTotal());
+        LOG.info("Total pages: {}", pageInfo.getPages());
 
         return CopyUtil.copyList(ebookList, EbookResp.class);
     }
