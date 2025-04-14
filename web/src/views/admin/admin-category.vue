6ce16341dae4cab7666f3fb5f9bbab5ed2a8f16b
vincent81 <rc.guo020801@outlook.com>
2025-04-13 21:47:28 -0400

    Implemented a select list when adding a new category.

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -57,6 +57,17 @@
       </a-form-item>
       <a-form-item label="Parent">
         <a-input v-model:value="category.parent" />
+        <a-select
+            v-model:value="category.parent"
+            ref="select"
+        >
+          <a-select-option :value="0">
+            New Category
+          </a-select-option>
+          <a-select-option v-for="c in level1" :key="c.id" :value="c.id" :disabled="category.id === c.id">
+            {{c.name}}
+          </a-select-option>
+        </a-select>
       </a-form-item>
       <a-form-item label="Order">
         <a-input v-model:value="category.sort " type="textarea" />
