bdf44537c1d00628f558a309e02159924668b888
vincent81 <rc.guo020801@outlook.com>
2025-04-14 12:34:12 -0400

    Default to showing the Welcome page. When clicking the Welcome menu item, show the Welcome page. When clicking a category menu item, show eBooks. Only second-level categories can trigger the click event.

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -21,7 +21,10 @@
         </a-menu>
       </a-layout-sider>
       <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-        <a-list item-layout="vertical" size="large" :grid="{gutter: 20, column: 3}" :data-source="ebooks">
+        <div class="welcome" v-show="isShowWelcome">
+          <h1>Welcome</h1>
+        </div>
+        <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{gutter: 20, column: 3}" :data-source="ebooks">
           <template #renderItem="{ item }">
             <a-list-item key="item.name">
               <template #actions>
@@ -106,8 +109,10 @@ export default defineComponent({
       });
     };
 
-    const handleClick = () => {
-      console.log("clicked");
+    const isShowWelcome = ref(true);
+
+    const handleClick = (value : any) => {
+      isShowWelcome.value = value.key == "welcome";
     };
 
     onMounted(() => {
@@ -136,6 +141,7 @@ export default defineComponent({
       handleClick,
       level1,
 
+      isShowWelcome,
     }
   }
 });
