08dee9ceddfe46ce222ff198d027b9aec01b8b32
vincent81 <rc.guo020801@outlook.com>
2025-05-03 14:12:27 -0400

    Add login form on the frontend.

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -21,7 +21,26 @@
       <a-menu-item key="5">
         <router-link to="/about">About me</router-link>
       </a-menu-item>
+      <a class="login-menu" v-show="!user.id" @click="showLoginModal">
+        <span>Login</span>
+      </a>
     </a-menu>
+
+    <a-modal
+        title="Login"
+        v-model:visible="loginModalVisible"
+        :confirm-loading="loginModalLoading"
+        @ok="login"
+    >
+      <a-form :model="loginUser" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+        <a-form-item label="Username">
+          <a-input v-model:value="loginUser.loginName" />
+        </a-form-item>
+        <a-form-item label="Password">
+          <a-input v-model:value="loginUser.password" type="password" />
+        </a-form-item>
+      </a-form>
+    </a-modal>
   </a-layout-header>
 </template>
 
@@ -30,5 +49,80 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'the-header',
+  setup () {
+    // Save after login
+    const user = computed(() => store.state.user);
+
+    // Login
+    const loginUser = ref({
+      loginName: "test",
+      password: "test"
+    });
+    const loginModalVisible = ref(false);
+    const loginModalLoading = ref(false);
+    const showLoginModal = () => {
+      loginModalVisible.value = true;
+    };
+
+    // Login
+    const login = () => {
+      console.log("Start logging");
+      loginModalLoading.value = true;
+      loginUser.value.password = hexMd5(loginUser.value.password + KEY);
+      axios.post('/user/login', loginUser.value).then((response) => {
+        loginModalLoading.value = false;
+        const data = response.data;
+        if (data.success) {
+          loginModalVisible.value = false;
+          message.success("Login successfully！");
+
+          store.commit("setUser", data.content);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    // Logout
+    const logout = () => {
+      console.log("Start logging out");
+      axios.get('/user/logout/' + user.value.token).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          message.success("Logout successfully！");
+          store.commit("setUser", {});
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    return {
+      loginModalVisible,
+      loginModalLoading,
+      showLoginModal,
+      loginUser,
+      login,
+      user,
+      logout
+    }
+  }
 });
 </script>
+
+<style>
+.logo {
+  width: 120px;
+  height: 31px;
+  /*background: rgba(255, 255, 255, 0.2);*/
+  /*margin: 16px 28px 16px 0;*/
+  float: left;
+  color: white;
+  font-size: 18px;
+}
+.login-menu {
+  float: right;
+  color: white;
+  padding-left: 10px;
+}
+</style>
