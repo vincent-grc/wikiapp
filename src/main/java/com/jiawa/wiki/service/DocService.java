9d2c4ae4fd9b754474a54d390ddc4773145d7232
vincent81 <rc.guo020801@outlook.com>
2025-05-12 15:28:28 -0400

    Add @Transacltional to ensure the atomicity and consistence of the save operation.

--- src/main/java/com/jiawa/wiki/service/DocService.java
+++ src/main/java/com/jiawa/wiki/service/DocService.java
@@ -26,6 +26,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 import java.util.List;
@@ -87,6 +88,7 @@ public class DocService {
     /**
      * Save the changes made from frontend
      */
+    @Transactional
     public void save(@Valid DocSaveReq req) {
         Doc doc = CopyUtil.copy(req, Doc.class);
         Content content = CopyUtil.copy(req, Content.class);
